992c33685e20962c638c74e59cdeec4868497885
Md Yamin <mdyamin007@yahoo.com>
2022-10-18 18:04:25 +0600

    storage changes to firebase

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,31 +1,20 @@
-const AWS = require('aws-sdk');
+const { v4 } = require("uuid");
+const app = require("../config/firebase");
+require("firebase/storage");
 
-const keys = require('../config/keys');
-
-exports.s3Upload = async image => {
-  let imageUrl = '';
-  let imageKey = '';
-
-  if (image) {
-    const s3bucket = new AWS.S3({
-      accessKeyId: keys.aws.accessKeyId,
-      secretAccessKey: keys.aws.secretAccessKey,
-      region: keys.aws.region
-    });
-
-    const params = {
-      Bucket: keys.aws.bucketName,
-      Key: image.originalname,
-      Body: image.buffer,
-      ContentType: image.mimetype,
-      ACL: 'public-read'
-    };
-
-    const s3Upload = await s3bucket.upload(params).promise();
-
-    imageUrl = s3Upload.Location;
-    imageKey = s3Upload.key;
-  }
-
-  return { imageUrl, imageKey };
+exports.uploadImage = (image) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const type = image.originalname.split(".")[1];
+      const imageKey = `${image.fieldname}${v4()}.${type}`;
+      const storage = app.storage().ref();
+      const imageRef = storage.child(imageKey);
+      const snapshot = await imageRef.put(image.buffer);
+      const imageUrl = await snapshot.ref.getDownloadURL();
+      resolve({ imageUrl, imageKey });
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
 };
